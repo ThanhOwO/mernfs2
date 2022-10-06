f5d05ed2e36344f3a673bec42d2775d1a267d056
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-10-06 21:06:56 +0700

    add more file

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
-
+import { useDispatch } from "react-redux";
+import { getPosts} from './actions/posts'
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
 
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">
+            <AppBar sx={{borderRadius: '15px',
+                        margin: '30px 0',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',}} position="static" color="inherit">
+                <Typography sx={{color: 'rgba(0, 183, 255, 1)'}} variant="h2" align="center">
                     Game Reviews
-                <img src="https://img.freepik.com/premium-vector/letter-c-game-logo-icon-vector-template_10060-695.jpg?w=2000" alt="" height="100" />
+                <img sx={{marginLeft: '15px'}} src="https://img.freepik.com/premium-vector/letter-c-game-logo-icon-vector-template_10060-695.jpg?w=2000" alt="" height="60" />
                 </Typography>
             </AppBar>
             <Grow in>
